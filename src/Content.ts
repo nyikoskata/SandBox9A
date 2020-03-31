2324cbd3d381452e07797bf38f69e826f5423187
nyikoskata <nykata04@gmail.com>
2020-03-31 11:04:29 +0200

    up

--- src/Content.ts
+++ src/Content.ts
@@ -25,10 +25,41 @@ export default class Content {
 
         // Kezd a kódolást innen -->
 
-        res.write("Hello Jedlik, hello 9A!");
-        res.write("<h2 style='color:green'>TypeScript</h2>");
-        res.write("próba\n");
-        res.write("alma\n");
+        // res.write("Hello Jedlik, hello 9A!");
+        // res.write("<h2 style='color:green'>TypeScript</h2>");
+        // res.write("próba\n");
+        // res.write("alma\n");
+
+        let x = 12; // változó definíció
+        x = 20;
+        res.write(`Az x változó érték: ${x}\n`);
+        res.write(x.toString() + "\n");
+        const szöveg = "alma";
+        res.write(szöveg + "\n");
+        let esik: boolean;
+        esik = true;
+        esik = false;
+        res.write(`${!esik}\n`);
+
+        res.write("Téglalap területe ls kerülete\n");
+        res.write("a= ");
+        let oldalA: number = parseInt(params.inputa as string);
+        if (isNaN(oldalA)) {
+            oldalA = 20;
+        }
+        res.write(`<input type='number' name='inputa' value=${oldalA} style='width:5em;' onChange='this.form.submit();'>\n`);
+
+        res.write("b= ");
+        let oldalB: number = parseInt(params.inputb as string);
+        if (isNaN(oldalB)) {
+            oldalB = 30;
+        }
+        res.write(`<input type='number' name='inputb' value=${oldalB} style='width:5em;' onChange='this.form.submit();'>\n`);
+        const terület = oldalA * oldalB;
+        const kerület = 2 * (oldalA + oldalB);
+        res.write(`Terület = ${terület} \n`);
+        res.write(`Terület = ${kerület} \n`);
+
         // <---- Fejezd be a kódolást
 
         res.write("</pre></form></body></html>");
